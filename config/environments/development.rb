d79fe4c2eb663a1646ba0041bab7117c79685904
Musharraf-2 <musharraf.tahir@devsinc.com>
2022-09-05 10:25:53 +0500

    Devise setup for user model

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,4 +58,16 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # Email Configuration
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    user_name: 'musharraf.tahir@devsinc.com',
+    password: 'wsogoxngmbvufdwn',
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 end
