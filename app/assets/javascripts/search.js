38028f2a179d318d18c3801e410b90503336b1e1
Musharraf-2 <musharraf.tahir@devsinc.com>
2022-09-19 15:26:09 +0500

    Rails Revaluation Points Completed ProductsHelper: added helper method for show_wishlist_button * ProductsHelper: added helper method for show_product_buy_button * ProductsHelper: added helper method for check_for_products_in_cart * Search.js: added styling for search bar

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,13 +1,13 @@
 document.addEventListener("turbolinks:load", function() {
   $('#search_key').on('input', function(e) {
     Rails.ajax({
-      url: './products/search?key=' + e.target.value,
+      url: '/products/search?key=' + e.target.value,
       type: "get",
       success: function(results) {
         console.log(results);
         $('#search').empty();
         results.forEach(function(result) {
-          $('#search').append('<a href="products/' + result.id + '">' + result.title + '</a><br />');
+          $('#search').append('<div style="background-color: white"><a style="text-decoration: none; color: black;" href="products/' + result.id + '">' + result.title + '</a></div><hr>');
          });
       },
       error: function(err) {
